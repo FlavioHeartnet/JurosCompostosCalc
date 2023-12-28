1cd33e0e3018bb33e847f1e9ba730f22c6162680
Flavio <flavionogueirabarros@gmail.com>
2023-12-27 22:09:04 -0300

    styling output and add more info, tax and tax rate

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readlineSync = require("readline-sync");
 function calcularJurosCompostos(valorInicial, aporteMensal, taxaJurosMensal, periodoAnos) {
     var taxaJurosDecimal = taxaJurosMensal / 100;
     var numeroPeriodos = periodoAnos * 12;
     var montante = valorInicial;
+    var rendimento = 0;
     for (var i = 1; i <= numeroPeriodos; i++) {
         montante = montante * (1 + taxaJurosDecimal) + aporteMensal;
+        rendimento = montante - valorInicial - aporteMensal * (i - 1);
     }
-    return formatarNumero(montante);
+    var investimentoDeduzidoImposto = calcImpostoSobrerendimento(rendimento, periodoAnos);
+    return [
+        formatarNumero(montante),
+        formatarNumero(rendimento),
+        formatarNumero(montante - rendimento),
+        formatarNumero((montante - rendimento) + investimentoDeduzidoImposto.rendimento),
+        investimentoDeduzidoImposto.aliquota.toFixed(1),
+        formatarNumero(rendimento - investimentoDeduzidoImposto.rendimento)
+    ];
 }
-var valorInicial = 2000;
-var aporteMensal = 500;
-var taxaJurosMensal = 0.946;
-var periodoAnos = 20;
+// Solicitar valores ao usuário
+var valorInicial = parseFloat(readlineSync.question('Digite o valor inicial: '));
+var aporteMensal = parseFloat(readlineSync.question('Digite o aporte mensal: '));
+var taxaJurosAnual = parseFloat(readlineSync.question('Digite a taxa de juros anual (decimal usando "." Ex: 5.94): '));
+var periodoAnos = parseFloat(readlineSync.question('Digite o período de investimento em anos: '));
+var taxaJurosMensal = taxaJurosAnual / 12;
 var resultado = calcularJurosCompostos(valorInicial, aporteMensal, taxaJurosMensal, periodoAnos);
 function formatarNumero(numero) {
     return numero.toFixed(2).replace(/\./g, ',').replace(/\d(?=(\d{3})+,)/g, '$&.');
 }
-console.log("O montante acumulado ao final de ".concat(periodoAnos, " anos \u00E9 R$").concat(resultado));
+function calcImpostoSobrerendimento(rendimento, periodoAnos) {
+    var aliquota = 0;
+    if (periodoAnos <= 0.6) {
+        aliquota = 22.5;
+    }
+    if (periodoAnos > 0.6 && periodoAnos < 1) {
+        aliquota = 20;
+    }
+    if (periodoAnos > 1 && periodoAnos < 2) {
+        aliquota = 17.5;
+    }
+    if (periodoAnos > 2) {
+        aliquota = 15;
+    }
+    var rendimentoDeduzidoImposto = rendimento - ((aliquota / 100) * rendimento);
+    return {
+        aliquota: aliquota,
+        rendimento: rendimentoDeduzidoImposto
+    };
+}
+console.log("\nO montante acumulado ao final de ".concat(periodoAnos, " anos \u00E9 \u001B[38;2;102;204;0mR$").concat(resultado[0], "\u001B[0m\n  \nValor Investido: \u001B[38;2;255;255;0mR$").concat(resultado[2], "\u001B[0m\n  \nRendimento: \u001B[38;2;0;128;255mR$").concat(resultado[1], "\u001B[0m\n  \nCaso este investimento n\u00E3o seja isento de IR, segue calculo de dedu\u00E7\u00E3o:  \n  \nValor total deduzindo imposto de renda: \u001B[38;2;128;225;0mR$").concat(resultado[3], "\u001B[0m  \n  \nValor retido na fonte: \u001B[38;2;255;51;51mR$").concat(resultado[5], "\u001B[0m  Aliquota: \u001B[38;2;255;51;51m").concat(resultado[4], "%\u001B[0m \n"));
--- main.ts
+++ main.ts
@@ -1,25 +1,38 @@
+import * as readlineSync from 'readline-sync';
+
 function calcularJurosCompostos(
   valorInicial: number,
   aporteMensal: number,
   taxaJurosMensal: number,
   periodoAnos: number
-): string {
+): string[] {
   const taxaJurosDecimal = taxaJurosMensal / 100;
   const numeroPeriodos = periodoAnos * 12;
   let montante = valorInicial;
-
+  let rendimento = 0;
   for (let i = 1; i <= numeroPeriodos; i++) {
     montante = montante * (1 + taxaJurosDecimal) + aporteMensal;
+    rendimento = montante - valorInicial - aporteMensal * (i - 1);
   }
 
-  return formatarNumero(montante);
+  const investimentoDeduzidoImposto = calcImpostoSobrerendimento(rendimento, periodoAnos);
+
+  return [ 
+    formatarNumero(montante), 
+    formatarNumero(rendimento), 
+    formatarNumero(montante - rendimento), 
+    formatarNumero((montante - rendimento) + investimentoDeduzidoImposto.rendimento), 
+    investimentoDeduzidoImposto.aliquota.toFixed(1),
+    formatarNumero(rendimento - investimentoDeduzidoImposto.rendimento) 
+  ];
 }
 
-const valorInicial = 2000;
-const aporteMensal = 500;
-const taxaJurosMensal = 0.946;
-const periodoAnos = 20;
-
+// Solicitar valores ao usuário
+const valorInicial = parseFloat(readlineSync.question('Digite o valor inicial: '));
+const aporteMensal = parseFloat(readlineSync.question('Digite o aporte mensal: '));
+const taxaJurosAnual = parseFloat(readlineSync.question('Digite a taxa de juros anual (decimal usando "." Ex: 5.94): '));
+const periodoAnos = parseFloat(readlineSync.question('Digite o período de investimento em anos: '));
+const taxaJurosMensal = taxaJurosAnual / 12;
 const resultado = calcularJurosCompostos(
   valorInicial,
   aporteMensal,
@@ -29,4 +42,37 @@ const resultado = calcularJurosCompostos(
 function formatarNumero(numero: number): string {
   return numero.toFixed(2).replace(/\./g, ',').replace(/\d(?=(\d{3})+,)/g, '$&.');
 }
-console.log(`O montante acumulado ao final de ${periodoAnos} anos é R$${resultado}`);
+
+type RendimentoAliquota = {
+  rendimento: number;
+  aliquota: number
+}
+
+function calcImpostoSobrerendimento(rendimento: number, periodoAnos: number):RendimentoAliquota  {
+  let aliquota = 0;
+  if(periodoAnos <= 0.6){
+    aliquota = 22.5;
+  }
+  if(periodoAnos > 0.6 && periodoAnos < 1) {
+    aliquota = 20;
+  }
+  if(periodoAnos > 1 && periodoAnos < 2) {
+    aliquota = 17.5;
+  }
+  if(periodoAnos > 2) {
+    aliquota = 15;
+  }
+  const rendimentoDeduzidoImposto = rendimento - ((aliquota / 100) * rendimento);
+  return {
+    aliquota: aliquota,
+    rendimento: rendimentoDeduzidoImposto
+  }
+}
+
+console.log(`\nO montante acumulado ao final de ${periodoAnos} anos é \x1b[38;2;102;204;0mR$${resultado[0]}\x1b[0m
+  \nValor Investido: \x1b[38;2;255;255;0mR$${resultado[2]}\x1b[0m
+  \nRendimento: \x1b[38;2;0;128;255mR$${resultado[1]}\x1b[0m
+  \nCaso este investimento não seja isento de IR, segue calculo de dedução:  
+  \nValor total deduzindo imposto de renda: \x1b[38;2;128;225;0mR$${resultado[3]}\x1b[0m  
+  \nValor retido na fonte: \x1b[38;2;255;51;51mR$${resultado[5]}\x1b[0m  Aliquota: \x1b[38;2;255;51;51m${resultado[4]}%\x1b[0m 
+`);
