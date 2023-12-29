60847b737b48c5b2289c5a6af4d36552fd84a058
Flavio <flavionogueirabarros@gmail.com>
2023-12-29 14:28:40 -0300

    adding come cotas

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,39 @@
 export function formatarNumero(numero: number): string {
     return numero.toFixed(2).replace(/\./g, ',').replace(/\d(?=(\d{3})+,)/g, '$&.');
   }
+
+  export function obterAliquotaComeCotas(diasInvestidos: number): number {
+    if (diasInvestidos <= 180) {
+      return 0.225; // 22,5%
+    } else if (diasInvestidos <= 360) {
+      return 0.20; // 20%
+    } else if (diasInvestidos <= 720) {
+      return 0.175; // 17,5%
+    } else {
+      return 0.15; // 15%
+    }
+  }
+export type RendimentoAliquota = {
+  rendimento: number;
+  aliquota: number
+}
+export function calcImpostoSobrerendimento(rendimento: number, periodoAnos: number): RendimentoAliquota  {
+    let aliquota = 0;
+    if(periodoAnos <= 0.6){
+      aliquota = 0.225; // 22,5%
+    }
+    if(periodoAnos > 0.6 && periodoAnos < 1) {
+      aliquota = 0.20; // 20%
+    }
+    if(periodoAnos > 1 && periodoAnos < 2) {
+      aliquota = 0.175; // 17,5%
+    }
+    if(periodoAnos > 2) {
+      aliquota = 0.15; // 15%
+    }
+    const rendimentoDeduzidoImposto = rendimento - (aliquota * rendimento);
+    return {
+      aliquota: aliquota * 100,
+      rendimento: rendimentoDeduzidoImposto
+    }
+  }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,40 @@
 import * as readlineSync from 'readline-sync';
 import { calcularJurosCompostosDetalhado, imprimirTabela } from './tabelaRendimentos';
-import { formatarNumero } from './helper';
+import { calcImpostoSobrerendimento, formatarNumero, obterAliquotaComeCotas, RendimentoAliquota } from './helper';
 
 function calcularJurosCompostos(
   valorInicial: number,
   aporteMensal: number,
   taxaJurosMensal: number,
-  periodoAnos: number
+  periodoAnos: number,
+  taxacorretagemAnual: number,
+  temComeCotas: number
 ): string[] {
   const taxaJurosDecimal = taxaJurosMensal / 100;
   const numeroPeriodos = periodoAnos * 12;
   let montante = valorInicial;
   let rendimento = 0;
+  const taxaCorretagemMensal = taxacorretagemAnual / 12 / 100; // Taxa de corretagem mensal
+  let  impostoPagoComeCotas = 0; // Valor total de impostos pagos no come cotas
+
   for (let i = 1; i <= numeroPeriodos; i++) {
     montante = montante * (1 + taxaJurosDecimal) + aporteMensal;
     rendimento = montante - valorInicial - aporteMensal * (i - 1);
+    montante = montante * (1 - taxaCorretagemMensal); // Deduz a taxa de corretagem
+    if(temComeCotas == 1){
+      if ((i + 1) % 6 === 0) {
+        // Aplica o come-cotas a cada 6 meses
+        const diasInvestidos = (i + 1) * 30; // Aproximadamente assumindo 30 dias por mês
+        const aliquotaComeCotas = obterAliquotaComeCotas(diasInvestidos);
+        const rendimentoSemImposto = montante - valorInicial - (aporteMensal * i);
+        const impostoComeCotas = rendimentoSemImposto * aliquotaComeCotas;
+        montante -= impostoComeCotas;
+        impostoPagoComeCotas += impostoComeCotas;
+      }
+    }
   }
+
+  
 
   const investimentoDeduzidoImposto = calcImpostoSobrerendimento(rendimento, periodoAnos);
 
@@ -25,7 +44,8 @@ function calcularJurosCompostos(
     formatarNumero((montante - rendimento) + aporteMensal), 
     formatarNumero((montante - rendimento) + investimentoDeduzidoImposto.rendimento), 
     investimentoDeduzidoImposto.aliquota.toFixed(1),
-    formatarNumero(rendimento - investimentoDeduzidoImposto.rendimento) 
+    formatarNumero(rendimento - investimentoDeduzidoImposto.rendimento),
+    formatarNumero(impostoPagoComeCotas) 
   ];
 }
 
@@ -33,6 +53,12 @@ function calcularJurosCompostos(
 const valorInicial = parseFloat(readlineSync.question('Digite o valor inicial: '));
 const aporteMensal = parseFloat(readlineSync.question('Digite o aporte mensal: '));
 const taxaJurosAnual = parseFloat(readlineSync.question('Digite a taxa de juros anual (decimal usando "." Ex: 5.94): '));
+const taxacorretagemAnual = parseFloat(readlineSync.question('Se existir taxa de corretagem digite aqui: (Ex: 0.7 ao ano): '));
+const temComeCotas = parseFloat(readlineSync.question('Seu investimento esta sujeito ao come cotas(renda fixa, cambiais ou multimercados)?: (1) Sim, (2) não:  '));
+if(temComeCotas <= 0 || temComeCotas > 2){
+  console.log('Opção invalida!!');
+  process.exit(1);
+}
 const periodoAnos = parseFloat(readlineSync.question('Período de investimento em anos (para meses Ex: 0.6 = 6 meses): '));
 //Converter para Juros mensais
 const taxaJurosMensal = taxaJurosAnual / 12;
@@ -40,7 +66,9 @@ const resultado = calcularJurosCompostos(
   valorInicial,
   aporteMensal,
   taxaJurosMensal,
-  periodoAnos
+  periodoAnos,
+  taxacorretagemAnual,
+  temComeCotas
 );
 
 const tabelaRendimentos = calcularJurosCompostosDetalhado(
@@ -50,33 +78,6 @@ const tabelaRendimentos = calcularJurosCompostosDetalhado(
   periodoAnos
 );
 
-type RendimentoAliquota = {
-  rendimento: number;
-  aliquota: number
-}
-
-function calcImpostoSobrerendimento(rendimento: number, periodoAnos: number):RendimentoAliquota  {
-  let aliquota = 0;
-  if(periodoAnos <= 0.6){
-    aliquota = 22.5;
-  }
-  if(periodoAnos > 0.6 && periodoAnos < 1) {
-    aliquota = 20;
-  }
-  if(periodoAnos > 1 && periodoAnos < 2) {
-    aliquota = 17.5;
-  }
-  if(periodoAnos > 2) {
-    aliquota = 15;
-  }
-  const rendimentoDeduzidoImposto = rendimento - ((aliquota / 100) * rendimento);
-  return {
-    aliquota: aliquota,
-    rendimento: rendimentoDeduzidoImposto
-  }
-}
-
-
 
 console.log(`\nO montante acumulado ao final de ${periodoAnos} anos é \x1b[38;2;102;204;0mR$${resultado[0]}\x1b[0m
   \nValor Investido: \x1b[38;2;255;255;0mR$${resultado[2]}\x1b[0m
@@ -85,8 +86,12 @@ console.log(`\nO montante acumulado ao final de ${periodoAnos} anos é \x1b[38;2
   \nValor total deduzindo imposto de renda: \x1b[38;2;128;225;0mR$${resultado[3]}\x1b[0m  
   \nValor retido na fonte: \x1b[38;2;255;51;51mR$${resultado[5]}\x1b[0m  Aliquota: \x1b[38;2;255;51;51m${resultado[4]}%\x1b[0m 
 `);
+if(parseFloat(resultado[6]) > 0){
+  console.log(`Valor retido no come cotas: \x1b[38;2;255;51;51mR$${resultado[6]}\x1b[0m`);
+}
 const meses: string[] = [];
 for (let i = 1; i <= periodoAnos * 12; i++) {
   meses.push(`${i}`);
 }
-imprimirTabela(meses, tabelaRendimentos.montantes, tabelaRendimentos.totais, tabelaRendimentos.valoresInvestidos);
+
+//imprimirTabela(meses, tabelaRendimentos.montantes, tabelaRendimentos.totais, tabelaRendimentos.valoresInvestidos);
